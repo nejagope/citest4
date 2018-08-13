3b26e753220ad550384bea56a538a9f7d93c5c0d
Nelson González <njgp86@gmail.com>
2018-08-13 12:08:40 -0600

    Agregada la clase principal

---
+++ citest4/src/main/java/com/ayd2/citest4/citest4.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ayd2.citest4;
+
+/**
+ *
+ * @author NELSONJAIR
+ */
+public class citest4 {
+    
+    public static void main(String [] args){
+        System.out.println("hola mundo");
+    }
+}
